8104da9386d12cdf464061663fba2ac9bf16bab6
shizzle444 <125227966+shizzle444@users.noreply.github.com>
2024-10-12 20:35:42 +0100

    Delete wblg2.jar.src directory

--- wblg2.jar.src/org/tbg/exceptions/AlreadyRunningException.java
+++
@@ -1,9 +0,0 @@
-package org.tbg.exceptions;
-
-public class AlreadyRunningException extends Exception {}
-
-
-/* Location:              C:\Users\shado\Desktop\WBLG chatgpt\wblg2.jar!\org\tbg\exceptions\AlreadyRunningException.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
